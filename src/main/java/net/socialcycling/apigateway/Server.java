eabc3ef115c49e04a7bcbd6e419071af4460d5bb
Artem Tsushko <artem.tsushko@gmail.com>
2016-06-12 19:41:32 +0300

    Set up keycloak auth

--- src/main/java/net/socialcycling/apigateway/Server.java
+++ src/main/java/net/socialcycling/apigateway/Server.java
@@ -3,7 +3,10 @@ package net.socialcycling.apigateway;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
 import io.vertx.core.json.JsonObject;
+import io.vertx.ext.auth.jwt.JWTAuth;
 import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
+import io.vertx.ext.web.handler.JWTAuthHandler;
 
 public class Server extends AbstractVerticle {
 
@@ -12,17 +15,11 @@ public class Server extends AbstractVerticle {
 
         Router router = Router.router(vertx);
 
-        router.route("/").handler(routingContext -> {
-            routingContext
-                    .response()
-                    .putHeader("content-type", "application/json")
-                    .end(
-                            new JsonObject()
-                            .put("from", "vert.x")
-                            .put("msg", "hello, socialcyling")
-                            .encodePrettily()
-                    );
-        });
+        JWTAuthHandler authHandler = JWTAuthHandler.create(JWTAuth.create(vertx,
+                new JsonObject().put("public-key", config().getString("keycloak.realm-public-key"))));
+
+        router.route("/protected/*").order(-1).handler(authHandler);
+        router.route("/protected/message").handler(this::handleHello);
 
         vertx
                 .createHttpServer()
@@ -36,6 +33,20 @@ public class Server extends AbstractVerticle {
                                 startFuture.fail(result.cause());
                             }
                         }
+                );
+    }
+
+    private void handleHello(RoutingContext routingContext) {
+        JsonObject principal = routingContext.user().principal();
+        routingContext
+                .response()
+                .putHeader("content-type", "application/json")
+                .end(
+                        new JsonObject()
+                                .put("from", "vert.x")
+                                .put("msg", "hello, " + principal.getString("name"))
+                                .put("principal", principal)
+                                .encodePrettily()
                 );
     }
 }

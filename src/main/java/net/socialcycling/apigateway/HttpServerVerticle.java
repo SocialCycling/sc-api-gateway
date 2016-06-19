d6f60955c26a233d12d84cdea462ae5a8239f023
Artem Tsushko <artem.tsushko@gmail.com>
2016-06-20 02:36:45 +0300

    Added geocoding request support (checking auth, finding Nominatim instance trough service discovery, then proxying client request to Nominatim)

--- src/main/java/net/socialcycling/apigateway/HttpServerVerticle.java
+++ src/main/java/net/socialcycling/apigateway/HttpServerVerticle.java
@@ -2,30 +2,40 @@ package net.socialcycling.apigateway;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
+import io.vertx.core.http.HttpClient;
+import io.vertx.core.http.HttpServerResponse;
 import io.vertx.core.json.JsonObject;
+import io.vertx.core.streams.Pump;
 import io.vertx.ext.auth.jwt.JWTAuth;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.handler.JWTAuthHandler;
+import io.vertx.servicediscovery.ServiceDiscovery;
+import io.vertx.servicediscovery.types.HttpEndpoint;
 
 public class HttpServerVerticle extends AbstractVerticle {
 
+    private ServiceDiscovery discovery;
+
+
     @Override
     public void start(Future<Void> startFuture) {
+
+        discovery = ServiceDiscovery.create(vertx);
 
         Router router = Router.router(vertx);
 
         JWTAuthHandler authHandler = JWTAuthHandler.create(JWTAuth.create(vertx,
                 new JsonObject().put("public-key", config().getString("keycloak.realm-public-key"))));
 
-        router.route("/protected/*").order(-1).handler(authHandler);
-        router.route("/protected/message").handler(this::handleHello);
+        router.get("/geocode").handler(authHandler);
+        router.get("/geocode").handler(this::handleGeocode);
 
         vertx
                 .createHttpServer()
                 .requestHandler(router::accept)
                 .listen(
-                        config().getInteger("http.port", 7175),
+                        config().getInteger("http.port"),
                         result -> {
                             if (result.succeeded()) {
                                 startFuture.complete();
@@ -36,17 +46,50 @@ public class HttpServerVerticle extends AbstractVerticle {
                 );
     }
 
-    private void handleHello(RoutingContext routingContext) {
-        JsonObject principal = routingContext.user().principal();
-        routingContext
-                .response()
-                .putHeader("content-type", "application/json")
-                .end(
-                        new JsonObject()
-                                .put("from", "vert.x")
-                                .put("msg", "hello, " + principal.getString("name"))
-                                .put("principal", principal)
-                                .encodePrettily()
-                );
+    @Override
+    public void stop() throws Exception {
+        if (discovery != null) {
+            discovery.close();
+        }
     }
+
+
+    private void handleGeocode(RoutingContext ctx) {
+        Double eps = config().getDouble("geocoding.viewbox.eps");
+        String query = "/search.php?viewbox="
+                + (Double.parseDouble(ctx.request().getParam("longitude")) - eps) + ","
+                + (Double.parseDouble(ctx.request().getParam("latitude")) - eps) + ","
+                + (Double.parseDouble(ctx.request().getParam("longitude")) + eps) + ","
+                + (Double.parseDouble(ctx.request().getParam("latitude")) + eps) + "&"
+                + "q=" + ctx.request().getParam("query") + "&"
+                + "format=" + config().getString("geocoding.format") + "&"
+                + "bounded=" + config().getInteger("geocoding.bounded") + "&"
+                + "limit=" + config().getInteger("geocoding.limit");
+        HttpServerResponse serverResponse = ctx.response();
+        HttpEndpoint.getClient(
+                discovery,
+                new JsonObject().put("name", "net.socialcycling.geocoding"),
+                geocoderDiscoveryResult -> {
+                    if (geocoderDiscoveryResult.succeeded()) {
+                        HttpClient geocoderClient = geocoderDiscoveryResult.result();
+                        geocoderClient.getNow(query, clientResponse -> {
+                            serverResponse
+                                    .putHeader("content-type", "application/json")
+                                    .setChunked(true);
+                            Pump.pump(clientResponse, serverResponse).start();
+                            clientResponse.endHandler(v -> {
+                                serverResponse.end();
+                                ServiceDiscovery.releaseServiceObject(discovery, geocoderClient);
+                            });
+                        });
+                    } else  {
+                        serverResponse
+                                .setStatusCode(500)
+                                .setStatusMessage("geocoding service unavailable")
+                                .end();
+                    }
+                }
+        );
+    }
+
 }
